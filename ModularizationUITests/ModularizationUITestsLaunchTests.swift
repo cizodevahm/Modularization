fccf2f9afd74a5802f4fecefbd01b1f6028e805f
Cizotech Software Pvt Ltd <93611338+cizodevahm@users.noreply.github.com>
2024-02-14 20:03:39 +0530

    Add files via upload

---
+++ ModularizationUITests/ModularizationUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ModularizationUITestsLaunchTests.swift
+//  ModularizationUITests
+//
+//  Created by jignesh solanki on 13/02/24.
+//
+
+import XCTest
+
+final class ModularizationUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
